2b9c570d82ea1c9581aaa89c1a5a9b95bacea617
unknown <sharansk792000@gmail.com>
2024-03-14 22:29:25 +0530

    updated

---
+++ abstraction.java
@@ -0,0 +1,7 @@
+/*Abstraction is a process of hiding implementation from the user and only
+ the highlighted set of services provided to the user is visible
+ in abstraction it is implemented by using abstract class
+ */
+public class abstraction {
+    
+}
